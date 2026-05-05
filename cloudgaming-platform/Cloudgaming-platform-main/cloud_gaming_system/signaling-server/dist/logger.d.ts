7f5e38d6ccf0382f4179a2f5a1edb9431f0c6485
Razerl66/77solardragon <118341283+the-real-razerl66@users.noreply.github.com>
2026-05-05 20:13:51 +1000

    deleted

--- cloudgaming-platform/Cloudgaming-platform-main/cloud_gaming_system/signaling-server/dist/logger.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Winston logger configuration
- */
-import winston from 'winston';
-declare const logger: winston.Logger;
-export default logger;
-//# sourceMappingURL=logger.d.ts.map
--- cloudgaming-platform/Cloudgaming-platform-main/cloud_gaming_system/signaling-server/dist/types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * Type definitions for the cloud gaming signaling server
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=types.js.map
